3b359ef6d1725e93471289a3384907460b6f9088
shenaishiren <gjw.jesus@qq.com>
2017-08-10 18:31:05 +0800

    init

--- fabfile.py
+++ fabfile.py
@@ -3,22 +3,24 @@
 from fabric.api import cd, run, local, env
 from fabric.contrib.files import exists
 
-code_path = "/root/product/xiaodi/xiaodi-api/"
-code_dir = "/root/product/xiaodi/xiaodi-api/xiaodi"
+code_path = "xxxx"
+code_dir = "xxxx"
 
 
 def remote_pull():
     with cd(code_dir):
         run("git pull")
 
+
 def local_pull():
     local("git pull")
+
 
 def dynamic_reload():
     with cd(code_dir):
         run("git pull")
         run("docker-compose build")
-        run("docker exec xiaodi_api_1 rm /code/xiaodi/celery_tasks/celerybeat.pid")
+        run("docker exec xiaodi_api_1 rm /xxx/celerybeat.pid")
         run("supervisorctl restart xiaodi_api")
 
 
